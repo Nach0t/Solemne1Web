a5ea7ad9b1cf926b86baf3b2f939d70b96579948
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2025-04-05 13:05:48 -0300

    update 1

--- README.md
+++
@@ -1,1 +0,0 @@
-# Solemne1Web
---
+++ main.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from fastapi.responses import RedirectResponse
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/time")
+
+
+@app.get(
+    "/time",
+    summary="Obtener la fecha y hora actual",
+    description="Devuelve la fecha y hora actual en formato JSON.",
+)
+async def get_time():
+    """
+    Retorna la fecha y hora actual en formato Año-Mes-Día Hora:Minuto:Segundo.
+    """
+    now = datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return {"time": formatted_time}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
